1f3baca3657bb77f17e5555912e8383c35ab650a
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-02-25 15:04:21 +0100

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+const theme = {
+  colors: {
+    main: 'silver',
+    button: 'grey',
+    good: 'green',
+    neutral: 'orange',
+    bad: 'red',
+    statistics: 'black',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
